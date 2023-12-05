66dd208359f295b8ed305915e81d51490fe82b3f
Oliver Isaksson <127422659+Bouduli@users.noreply.github.com>
2023-12-05 10:07:26 +0100

    Added assignment 2. Today we try rust

---
+++ 2/target/debug/deps/_2-de4b1f73484d8b47.d
@@ -0,0 +1,5 @@
+/home/oliver/projects/advent-of-code/advent-of-code-2023/2/target/debug/deps/_2-de4b1f73484d8b47.rmeta: src/main.rs
+
+/home/oliver/projects/advent-of-code/advent-of-code-2023/2/target/debug/deps/_2-de4b1f73484d8b47.d: src/main.rs
+
+src/main.rs:
---
+++ 2/target/debug/deps/_2-ea271ff5b8a87ada.d
@@ -0,0 +1,5 @@
+/home/oliver/projects/advent-of-code/advent-of-code-2023/2/target/debug/deps/_2-ea271ff5b8a87ada.rmeta: src/main.rs
+
+/home/oliver/projects/advent-of-code/advent-of-code-2023/2/target/debug/deps/_2-ea271ff5b8a87ada.d: src/main.rs
+
+src/main.rs:
