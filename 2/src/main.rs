0091ef38a434d3abfbe8d55aa3d4f4e37b4d0f0c
Oliver Isaksson <127422659+Bouduli@users.noreply.github.com>
2023-12-05 12:00:02 +0100

    Trying to make number two

--- 2/src/main.rs
+++ 2/src/main.rs
@@ -1,3 +1,25 @@
+use std::{fs, vec};
+
 fn main() {
     println!("Hello, world!");
+
+    // fs::read_to_string("src/aoc2_test");
+    let input: String = fs::read_to_string("src/aoc2_input").unwrap();
+    let lines =input.lines();
+    
+    for s in lines{
+        lookAtString(s);
+    }
+
+    // println!("{}",input);
+}
+
+fn lookAtString(string: &str)->bool{
+    let game: &str = string.split(": ").nth(1).unwrap();
+
+    let games: Vec<&str> = game.split(";").into_iter().collect();
+    print!("{:?}", games);
+
+
+    false
 }
